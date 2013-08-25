e38768658b1e5c80c0db9aecb9356969e77dbd92
Nikita Harlov <nikita@harlov.com>
2013-08-25 10:44:15 +0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homeblog::Application.config.secret_token = 'ef9df75ad65b558819b0a9827709cf243f5a8a6cbeb992067c39cf7d4b4587c25b00686c74cad08cba63a50e04ba1dbab333d4eb5e011c7065e5d588516acd50'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Homeblog::Application.config.session_store :cookie_store, key: '_homeblog_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Homeblog::Application.config.session_store :active_record_store
