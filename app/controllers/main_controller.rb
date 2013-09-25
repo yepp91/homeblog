47a3bd19c493797d7a279e5f722b1fed12fb6427
Nikita Harlov <nikita@harlov.com>
2013-09-25 08:19:16 +0400

    +

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -9,7 +9,7 @@ class MainController < ApplicationController
 
 	def how_time
 		now = Time.new
-		work_end = Time.mktime(now.year,now.month,now.day,1,0,0)		
+		work_end = Time.mktime(now.year,now.month,now.day,15,0,0)		
 		@diff_hour = ((work_end.to_i - now.to_i)/3600).abs
 		@diff_mins = ((work_end.to_i - now.to_i)%3600)/60
 		@work_end = work_end
