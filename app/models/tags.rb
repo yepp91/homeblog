6f8d8aaed8f7c8870d11ba1ebc7c95ef2c1e419e
Nikita Harlov <nikita@harlov.com>
2014-01-20 14:15:24 +0400

    +

--- app/models/tags.rb
+++ app/models/tags.rb
@@ -1,10 +1,12 @@
 class Tags < ActiveRecord::Base
   attr_accessible :name
 
-  def add(:tagname)
+  =begin
+def add(:tagname)
   	tag = Tags.find_by_name(:tagname)
   	if tag.nil?
   		tag = Tags.new([:name => :tagname])  	
-  end
+  end=end
+
 
 end
